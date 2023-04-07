d761128a668ea44ea51317f25661d64288ee7f2f
LetiSanti <117988796+LetiSanti@users.noreply.github.com>
2023-04-07 21:00:27 +0200

    Update tethavariable2cmN.m

--- tethavariable2cmN.m
+++ tethavariable2cmN.m
@@ -2,22 +2,26 @@ clear all
 close all
 clc
 
+%% valori misurati
 theta = linspace(-pi/15, pi/15, 17);
 voltage = [2.49 2.49 2.5 2.52 2.554 2.6 2.644 2.68 2.706 2.708 2.694 ...
     2.66 2.61 2.568 2.53 2.5 2.49]
-
+    
+%% assi di riferimento per lo spicchio di nostro interesse
 x = [0 -pi/15];
 y = [0 3];
 
 z = [0 pi/15];
 w = [0 3];
 
+%% curve per individuare i valori a regime
 a = linspace(-pi/15, -pi, 12);
 b = 2.49.*1.^(1:12);
 
 c = linspace (pi/15, pi, 12);
 d =2.49.*1.^(1:12);
 
+%% grafici
 figure(1)
 PolarGraph = polar(theta,voltage);
 PolarGraph.LineStyle ="-";
@@ -34,6 +38,7 @@ A.LineWidth = 1;
 B.Color = [0.3010 0.7450 0.9330];
 B.LineWidth = 1;
 
+%% caratteristiche titolo
 [t,s] = title ('Rate of change of the voltage with respect to degree, North pole','Distance set at 2 cm, \theta variable')
 t.FontSize = 16;
 s.FontAngle='italic';
