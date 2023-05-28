72ad741654ec7c351aca8b3241d02681476f2c2f
Alberto Pettenella <91367251+albptt@users.noreply.github.com>
2023-05-28 14:32:55 +0200

    Add files via upload

--- Arduino Code/RPM_algorithm.ino
+++ Arduino Code/RPM_algorithm.ino
@@ -2,13 +2,15 @@
 // RPMs are derived from the time delay between subsequent pulses
 
 #define SENS_PIN 2
-#define N 1 // moving window dimension
+#define N 1 // with N=1 it uses only the last period, if N increases it will do the floating window
+#define MAXRPM 520 // max possible value for RPM 
 
 #include <SoftwareSerial.h>
 #include <String.h>
+
 SoftwareSerial bluetooth(0,1);
 
-float timeOut = 0;
+float tempo_atteso=0;
 int RPM = 0;
 float RPS = 0.0;
 float Period[N]; // period between pulses
@@ -17,7 +19,9 @@ double timeNow = 0.0;
 double timeCurrent = 0.0; // current micros
 double sumPeriods = 0.0; // sum of the periods
 double avgPeriod = 0.0; // average of the periods
+
 int firstPulse=0;
+
 String data;
 
 void setup() {
@@ -26,12 +30,12 @@ void setup() {
   pinMode(SENS_PIN,INPUT);
   // attach interrupt to detect pulses
   attachInterrupt(digitalPinToInterrupt(SENS_PIN), pulseEvent, RISING);
-  // initialize baud rate for transmission
+  // start serial transmission
   Serial.begin(9600);
   bluetooth.begin(9600);
   // add delay to avoid random transmission
   delay(100);
-  // initialize window
+
   for(int i=0; i<N; i++)
     Period[i]=0;
 }
@@ -39,36 +43,44 @@ void setup() {
 void loop() {
   // compute time from last measurement
   timeCurrent = micros();
-  // update timeOut from last measurement
-  timeOut = (60.0 / (RPM+1))* 1.3 * 1000000;
-  // check for timeOut
-  if(timeCurrent - timeFlag > timeOut){
+
+  tempo_atteso=(60.0/(RPM+1))*1.3*1000000; // 30% longer than the time it took to make the last lap
+
+  if(timeCurrent - timeFlag > tempo_atteso){
     RPM=0;
   }
-  // send data via Bluetooth
-  data = String(RPM) + "\n";
+  
+  data = String(RPM)+"\n";
+
   bluetooth.print(data);
 }
 
 void pulseEvent(){
-  // save current time
-  timeNow = micros();
-  // shift window
-  for(int i=0; i < N-1; i++)
+
+  timeNow=micros();
+
+  for(int i=0; i<N-1; i++)
     Period[i]=Period[i+1];
-  // compute period between pulses
+  
   Period[N-1] = timeNow - timeFlag; 
+  
   // update time of measurement
   timeFlag = timeNow;
+
   // update sum of periods
-  for(int i=0; i < N; i++)
+  for(int i=0; i<N; i++)
     sumPeriods+=Period[i];
-  // compute average period between pulses
-  avgPeriod = sumPeriods / N;
+
+  avgPeriod = sumPeriods/N;
+
   // convert frequency to RPS
-  RPS = 1000000 / avgPeriod;
+  RPS = 1000000/avgPeriod;
   // convert RPS to RPM
-  RPM = RPS * 60;
-  // reset sumPeriods
+  RPM = RPS*60;
+
+  if(RPM > MAXRPM)
+    RPM = MAXRPM;
+
   sumPeriods = 0.0;
+
 }
